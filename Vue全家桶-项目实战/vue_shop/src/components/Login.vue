c186342d21f8f450538edcb58dca86c7aef9132d
MoMoOwO <strangersj@outlook.com>
2020-04-10 22:17:39 +0800

    项目清理与登录组件Demo及路由

---
+++ Vue全家桶-项目实战/vue_shop/src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>登陆组件</div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+
+  data () {
+    return {}
+  },
+
+  computed: {},
+
+  watch: {},
+
+  mounted () { },
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scope>
+</style>
