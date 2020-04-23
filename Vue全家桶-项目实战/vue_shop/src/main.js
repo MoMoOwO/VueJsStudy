13477b0ad25f0ddcd8d1da348f77b3bf12127023
MoMoOwO <strangersj@outlook.com>
2020-04-23 23:06:43 +0800

    axios请求拦截与组件导入

--- Vue全家桶-项目实战/vue_shop/src/main.js
+++ Vue全家桶-项目实战/vue_shop/src/main.js
@@ -12,6 +12,13 @@ import Axios from 'axios'
 import VueAxios from 'vue-axios'
 // 配置请求根路径
 Axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 配置 Axios 请求拦截器
+Axios.interceptors.request.use(config => {
+  // console.log(config)
+  // 为请求头对象添加 Token 验证的 Authorization 字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 // 配置 axios
 Vue.use(VueAxios, Axios)
 
