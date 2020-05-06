caee1ef51e9a7f30a30ec03ffa87beaabd281c93
MoMoOwO <strangersj@outlook.com>
2020-05-06 23:25:28 +0800

    完成商品分类列表展示

--- Vue全家桶-项目实战/vue_shop/src/components/goods/Cate.vue
+++ Vue全家桶-项目实战/vue_shop/src/components/goods/Cate.vue
@@ -14,8 +14,50 @@
         </el-col>
       </el-row>
       <!-- 分类表格 -->
+      <tree-table
+        class="treeTable"
+        :data="cateList"
+        :columns="columns"
+        :selection-type="false"
+        :expand-type="false"
+        show-index
+        index-text
+        border
+        :show-row-hover="false"
+      >
+        <!-- 是否有效列的模板 -->
+        <template slot="isok" slot-scope="scope">
+          <i
+            class="el-icon-success"
+            v-if="scope.row.cat_deleted === false"
+            style="color: lightgreen;"
+          ></i>
+          <i class="el-icon-error" v-else style="color: red;"></i>
+        </template>
+        <!-- 排序列的模板 -->
+        <template slot="order" slot-scope="scope">
+          <el-tag size="mini" v-if="scope.row.cat_level === 0">一级</el-tag>
+          <el-tag size="mini" type="success" v-else-if="scope.row.cat_level === 1">二级</el-tag>
+          <el-tag size="mini" type="warning" v-else>三级</el-tag>
+        </template>
+        <!-- 操作列模板 -->
+        <!-- <template slot="opt" slot-scope="scope"> -->
+        <template slot="opt">
+          <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
+          <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
+        </template>
+      </tree-table>
 
       <!-- 分页区域 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[3, 5, 10, 15]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
     </el-card>
   </div>
 </template>
@@ -27,8 +69,43 @@ export default {
 
   data () {
     return {
-
+      // 商品列表查询条件
+      queryInfo: {
+        type: 3,
+        pagenum: 1,
+        pagesize: 5
+      },
+      // 商品分类的数据列表，默认为空
+      cateList: [],
+      // 总数据条数
+      total: 0,
+      // 为 tree-table 指定列的定义
+      columns: [{
+        label: '分类名称',
+        prop: 'cat_name'
+      }, {
+        label: '是否有效',
+        prop: 'cat_deleted',
+        // 表示将当前页定义为模板列
+        type: 'template',
+        // 表示当前这一列使用的模板名称
+        template: 'isok'
+      },
+      {
+        label: '排序',
+        prop: 'cat_level',
+        type: 'template',
+        template: 'order'
+      }, {
+        label: '操作',
+        type: 'template',
+        template: 'opt'
+      }]
     }
+  },
+
+  created () {
+    this.getCateList()
   },
 
   computed: {},
@@ -37,10 +114,35 @@ export default {
 
   mounted () { },
 
-  methods: {}
+  methods: {
+    async getCateList () {
+      const { data: res } = await this.axios.get('categories', { params: this.queryInfo })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品分类列表失败！')
+      } else {
+        // 把数据列表赋值给 cateList
+        this.cateList = res.data.result
+        // 为总数据条数赋值
+        this.total = res.data.total
+      }
+    },
+    // 监听 pagesize 改变的事件
+    handleSizeChange (newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getCateList()
+    },
+    // 监听 pagenum 的改变
+    handleCurrentChange (newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getCateList()
+    }
+  }
 }
 
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+.treeTable {
+  margin-top: 15px;
+}
 </style>
