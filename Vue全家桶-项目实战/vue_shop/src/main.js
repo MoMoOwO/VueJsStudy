9654f4d241279147829f853eea264bf7a22f21c9
MoMoOwO <strangersj@outlook.com>
2020-05-11 20:39:51 +0800

    依赖添加与更新

--- Vue全家桶-项目实战/vue_shop/src/main.js
+++ Vue全家桶-项目实战/vue_shop/src/main.js
@@ -8,6 +8,12 @@ import './assets/css/global.css'
 import 'element-ui/lib/theme-chalk/index.css'
 // 导入表格插件
 import TreeTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器组件
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器组件样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 // 导入axios
 import Axios from 'axios'
@@ -28,6 +34,23 @@ Vue.config.productionTip = false
 
 // 注册第三方组件，全局可用的组件
 Vue.component('tree-table', TreeTable)
+// 将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+// 注册全局的时间过滤器
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
