0884d5e9849d23313fd231484cbe9dcff71adda7
MoMoOwO <strangersj@outlook.com>
2020-05-12 18:15:58 +0800

    实现路由懒加载

--- Vue全家桶-项目实战/vue_shop/src/router/index.js
+++ Vue全家桶-项目实战/vue_shop/src/router/index.js
@@ -1,17 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
+
+/* import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
-import Welcome from '../components/Welcome.vue'
-import Users from '../components/user/Users.vue'
+import Welcome from '../components/Welcome.vue' */
+
+const Login = () => import( /* webpackChunkName: "login_home_welcome" */ '../components/Login.vue')
+const Home = () => import( /* webpackChunkName: "login_home_welcome" */ '../components/Home.vue')
+const Welcome = () => import( /* webpackChunkName: "login_home_welcome" */ '../components/Welcome.vue')
+
+/* import Users from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
-import Roles from '../components/power/Roles.vue'
-import Cate from '../components/goods/Cate.vue'
-import Params from '../components/goods/Params.vue'
-import GoodsList from '../components/goods/List.vue'
-import AddGood from '../components/goods/Add.vue'
-import OrderList from '../components/order/Order.vue'
-import Report from '../components/report/Report.vue'
+import Roles from '../components/power/Roles.vue' */
+const Users = () => import( /* webpackChunkName: "users_rights_roles" */ '../components/user/Users.vue')
+const Rights = () => import( /* webpackChunkName: "users_rights_roles" */ '../components/power/Rights.vue')
+const Roles = () => import( /* webpackChunkName: "users_rights_roles" */ '../components/power/Roles.vue')
+
+/* import Cate from '../components/goods/Cate.vue'
+import Params from '../components/goods/Params.vue' */
+const Cate = () => import( /* webpackChunkName: "cate_params" */ '../components/goods/Cate.vue')
+const Params = () => import( /* webpackChunkName: "cate_params" */ '../components/goods/Params.vue')
+
+/* import GoodsList from '../components/goods/List.vue'
+import AddGood from '../components/goods/Add.vue' */
+const GoodsList = () => import( /* webpackChunkName: "goodslist_addgood" */ '../components/goods/List.vue')
+const AddGood = () => import( /* webpackChunkName: "goodslist_addgood" */ '../components/goods/Add.vue')
+
+/* import OrderList from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue' */
+const OrderList = () => import( /* webpackChunkName: "orderlist_report" */ '../components/order/Order.vue')
+const Report = () => import( /* webpackChunkName: "orderlist_report" */ '../components/report/Report.vue')
 
 Vue.use(VueRouter)
 
