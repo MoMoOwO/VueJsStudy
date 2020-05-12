78ab019eb37cc64a7752e769409c2f1f2c8309d8
MoMoOwO <strangersj@outlook.com>
2020-05-12 18:16:31 +0800

    安装配置babel插件

--- Vue全家桶-项目实战/vue_shop/babel.config.js
+++ Vue全家桶-项目实战/vue_shop/babel.config.js
@@ -1,5 +1,15 @@
+const prodPlugins = [] // 这是项目发布阶段需要用道德 babel 插件
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    // 发布产品时候的插件数组
+    ...prodPlugins,
+    // 声明路由懒加载插件
+    '@babel/plugin-syntax-dynamic-import'
   ]
 }
