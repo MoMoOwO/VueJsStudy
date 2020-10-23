23f339e8d50b66339dfd7b1eee128b0682c1e3fd
strangersj <strangersj@outlook.com>
2020-10-23 17:09:33 +0800

    格式调整

---
+++ Nuxtjs/nuxtdemo/pages/user.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>你好呀！</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: [],
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style lang="less" scoped></style>
