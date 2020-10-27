80f8700af0ced7ae92fd784a1b7a5ac23ed4985c
strangersj <strangersj@outlook.com>
2020-10-27 22:04:51 +0800

    nuxt 路由及路由传参

---
+++ Nuxtjs/nuxtdemo/pages/about/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>About Index page</h2>
+    <ul>
+      <li><nuxt-link :to="{ name: 'index' }">Home</nuxt-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: [],
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style lang="less" scoped></style>
---
+++ Nuxtjs/nuxtdemo/pages/news/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>News Index page</h2>
+    <p>NewsID: {{ $route.params.newsId }}</p>
+    <ul>
+      <li><nuxt-link :to="{ name: 'index' }">Home</nuxt-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: [],
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style lang="less" scoped></style>
