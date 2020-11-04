9606a9bc00f268d3e4c4fe4a3fc83e281df8076a
strangersj <strangersj@outlook.com>
2020-11-04 22:23:19 +0800

    错误页面

---
+++ Nuxtjs/nuxtdemo/layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2 v-if="error.statusCode == 404">404 您访问的页面不存在</h2>
+    <h2 v-else>500 服务器错误</h2>
+    <ul>
+      <li><nuxt-link to="/">Home</nuxt-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped></style>
